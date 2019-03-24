6ebb1d68d4a0b3a2f0fd6fb30731588685657bbd
AnatoliyShumov <shumov@radioday.kpi.ua>
2019-03-24 20:43:19 +0200

    fixed animation bag

--- UWATCH/shumov-app/src/components/counter.js
+++ UWATCH/shumov-app/src/components/counter.js
@@ -27,53 +27,56 @@ class Counter extends Component{
     loop = () => {
         const{to, from} = this.state;
         const {fps} = this.props;
-
-        setTimeout(()=> {
-            if ( this.frameId ) {
+        if (this.frameId ) {
+            setTimeout(()=> {
                 this.frameId = window.requestAnimationFrame( this.loop );
                 this.counter()
-                this.setState({
-                    frame: this.frameId
-                })
-            }
-            if(from >= to *0.01 && this.state.from <=this.state.to *0.8) {
-                this.setState({fps:fps})
-            }
-            if(from >=to *0.7 && this.state.from <=to *0.8) {
-                this.setState({fps:50})
+                if(from >= to *0.01 && this.state.from <=this.state.to *0.8) {
+                    this.setState({fps:fps})
+                }
+                else if(from >=to *0.7 && this.state.from <=to *0.8) {
+                    this.setState({fps:50})
 
-            }
-            if(from >= to *0.8 && from <=to *0.9) {
-                this.setState({fps:30})
+                }
+                else if(from >= to *0.8 && from <=to *0.9) {
+                    this.setState({fps:20})
 
-            }
-            if(from >=to *0.95 && from <=to *0.98) {
-                this.setState({fps:15})
+                }
+                else if(from >=to *0.95 && from <=to *0.98) {
+                    this.setState({fps:10})
 
-            }
-            if(from >=to*0.98 && this.state.from <=to *0.9) {
-                this.setState({fps:10})
+                }
+                else if(from >=to*0.98 && this.state.from <=to *0.9) {
+                    this.setState({fps:5})
+                }
 
-            }
-            else if (to > 0 && this.state.to < 101){
-                this.setState({countUp: 1})
-            }
-            else if(this.state.to > 100 && this.state.to < 1001){
-                this.setState({countUp: 10})
-            }
-            else if(this.state.to > 1000 && this.state.to < 10001){
-                this.setState({countUp: 100})
-            }
-            else if(this.state.to > 10000 && this.state.to < 10000001){
-                this.setState({countUp: 1223})}
-        }, 1000/this.state.fps)
+                else if (to > 0 && this.state.to < 101){
+                    this.setState({countUp: 1})
+                }
+                else if(this.state.to > 100 && this.state.to < 1001){
+                    this.setState({countUp: 10})
+                }
+                else if(this.state.to > 1000 && this.state.to < 10001){
+                    this.setState({countUp: 100})
+                }
+                else if(this.state.to > 10000 && this.state.to < 10000001){
+                    this.setState({countUp: 400})}
+console.log(1)
+            }, 800/this.state.fps)
+            this.setState({
+                frame: this.frameId
+            })
+        }
+
     }
 
     counter = () => {
         const {from, to, countUp} = this.state;
 
         if(from < to) {
-            this.setState(prevState=>({from: prevState.from + countUp}))}
+                this.setState(prevState=>({from: prevState.from + countUp}))
+        }
+
         else {
             this.stopLoop();
             this.setState({from: this.state.to})
